f0a6202d8e4b5939467371214bd604bfdb9cdd63
kallard <kallard@carnegielearning.com>
2025-08-24 07:21:12 -0400

    Update learnosity-init.js

--- netlify/functions/learnosity-init.js
+++ netlify/functions/learnosity-init.js
@@ -1,57 +1,22 @@
 // File Path: netlify/functions/learnosity-init.js
+// THIS IS A DIAGNOSTIC SCRIPT
 
-// ** THE FINAL FIX IS HERE: Use destructuring to get the Learnosity class **
-const { Learnosity } = require('learnosity-sdk-nodejs');
+const LearnositySdkModule = require('learnosity-sdk-nodejs');
 
 exports.handler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
-    }
+    
+    // THE ONLY PURPOSE of this function is to log the structure of the imported module.
+    console.log("--- DIAGNOSING LEARNOSITY SDK MODULE ---");
+    console.log("Type of imported module:", typeof LearnositySdkModule);
+    console.log("Keys available on the module:", Object.keys(LearnositySdkModule));
+    console.log("Full module structure:", JSON.stringify(LearnositySdkModule, null, 2));
+    console.log("--- END OF DIAGNOSIS ---");
 
-    try {
-        const { item_reference } = JSON.parse(event.body);
-        if (!item_reference) {
-            return { 
-                statusCode: 400, 
-                body: JSON.stringify({ error: 'Item reference is required.' }) 
-            };
-        }
-
-        const consumerKey = process.env.LEARNOSITY_CONSUMER_KEY;
-        const consumerSecret = process.env.LEARNOSITY_CONSUMER_SECRET;
-        
-        const headers = event.headers;
-        const domain = headers['x-forwarded-host'] || 'localhost';
-
-        // Now that the import is correct, creating an instance will work properly.
-        const learnositySdk = new Learnosity();
-        
-        const request = {
-            user_id: '$ANONYMIZED_USER_ID',
-            // And now the SDK's own utils will work, so we don't need the crypto workaround.
-            session_id: learnositySdk.utils.uuid(), 
-            domain: domain,
-            items: [item_reference],
-            rendering_type: 'inline'
-        };
-
-        // Calling .init() on the correctly formed instance will now succeed.
-        const signedRequest = learnositySdk.init('items', {
-            consumer_key: consumerKey,
-            consumer_secret: consumerSecret
-        }, request);
-
-        return {
-            statusCode: 200,
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(signedRequest)
-        };
-
-    } catch (error) {
-        console.error('Error in Learnosity function:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Could not process Learnosity request.' })
-        };
-    }
+    // We will return an error intentionally so we can see the log.
+    return {
+        statusCode: 500,
+        body: JSON.stringify({
+            error: 'This is a diagnostic run. Please check the function logs on Netlify.'
+        })
+    };
 };
